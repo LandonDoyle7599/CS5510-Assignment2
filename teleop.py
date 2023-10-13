2ad98e77101eee54b9209cf2ec6627f5a66bd60a
LandonDoyle7599 <landondoyle7599@gmail.com>
2023-10-12 19:29:35 -0600

    files

---
+++ teleop.py
@@ -0,0 +1,36 @@
+from Car import Car
+import readchar
+import cv2
+
+if __name__ == '__main__':
+    car = Car()
+    left = 50
+    right = 50
+    #initialize recording of video to save in a file
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('video.avi', fourcc, 20.0, (640, 480))
+
+    while True:
+        car.stop()
+        key = readchar.readkey()
+        if key == 'w':
+            car.control_car(left, right)
+        elif key == "s":
+            car.control_car(-left, -right)
+        elif key == "a":
+            car.control_car(-left, right)
+        elif key == "d":
+            car.control_car(left, -right)
+        elif key == "+":
+            left += 25
+            right += 25
+        elif key == "-":
+            left -= 25
+            right -= 25
+        elif key == "space":
+            car.stop()
+        elif key == "x":
+            car.stop()
+            break
+        else:
+            car.stop()
