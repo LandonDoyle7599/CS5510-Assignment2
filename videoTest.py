5b8b3c04735c6f6ebd348d2b4b7e0d6ecbf6ac9b
LandonDoyle7599 <landondoyle7599@gmail.com>
2023-10-12 21:24:24 -0600

    changes

--- videoTest.py
+++ videoTest.py
@@ -1,23 +1,46 @@
-import cv2
-
-# Create a VideoCapture object to access the camera
-cap = cv2.VideoCapture(0)  # Use 0 as the argument for the primary camera
-
-# Check if the camera is opened successfully
-if not cap.isOpened():
-    print("Error: Could not open camera.")
-else:
-    ret, frame = cap.read()  # Read a frame from the camera
-
-    if ret:
-        # Save the captured frame as an image (e.g., "captured_image.jpg")
-        cv2.imwrite("captured_image.jpg", frame)
-        print("Image captured and saved as 'captured_image.jpg'")
-    else:
-        print("Error: Could not capture an image.")
-
-    # Release the VideoCapture object
-    cap.release()
-
-# Close all OpenCV windows
-cv2.destroyAllWindows()
+# Python program to illustrate  
+# saving an operated video 
+  
+# organize imports 
+import numpy as np 
+import cv2 
+  
+# This will return video from the first webcam on your computer. 
+cap = cv2.VideoCapture(0)   
+  
+# Define the codec and create VideoWriter object 
+fourcc = cv2.VideoWriter_fourcc(*'XVID') 
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480)) 
+  
+# loop runs if capturing has been initialized.  
+while(True): 
+    # reads frames from a camera  
+    # ret checks return at each frame 
+    ret, frame = cap.read()  
+  
+    # Converts to HSV color space, OCV reads colors as BGR 
+    # frame is converted to hsv 
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
+      
+    # output the frame 
+    out.write(hsv)  
+      
+    # The original input frame is shown in the window  
+    cv2.imshow('Original', frame) 
+  
+    # The window showing the operated video stream  
+    cv2.imshow('frame', hsv) 
+  
+      
+    # Wait for 'a' key to stop the program  
+    if cv2.waitKey(1) & 0xFF == ord('a'): 
+        break
+  
+# Close the window / Release webcam 
+cap.release() 
+  
+# After we release our webcam, we also release the output 
+out.release()  
+  
+# De-allocate any associated memory usage  
+cv2.destroyAllWindows() 
